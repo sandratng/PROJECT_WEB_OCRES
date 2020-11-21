f5fe009ef6678fe123b1c538201c6bc154ac5495
sandratang <sandratang94@gmail.com>
2020-11-21 18:01:43 +0100

    début design

---
+++ frontend/src/layouts/MainLayout/TopBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import {
+  AppBar,
+  Toolbar,
+  makeStyles
+} from '@material-ui/core';
+
+const useStyles = makeStyles(({
+  root: {},
+  toolbar: {
+    height: 64
+  }
+}));
+
+const TopBar = ({ className, ...rest }) => {
+  const classes = useStyles();
+
+  return (
+    <AppBar
+      className={clsx(classes.root, className)}
+      elevation={0}
+      {...rest}
+    >
+      <Toolbar className={classes.toolbar}>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+TopBar.propTypes = {
+  className: PropTypes.string
+};
+
+export default TopBar;
