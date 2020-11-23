32b9671721a5b43510bc31ea6b02229f2bdbdd88
graceyt20 <graceyt20@gmail.com>
2020-11-23 02:12:22 +0100

    début solde bancaire

---
+++ frontend/src/views/reports/DashboardView/CompteBancaire.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import clsx from 'clsx';
+import {
+  Box,
+  Card,
+  CardHeader,
+  CardContent,
+  Divider,
+  makeStyles
+} from '@material-ui/core';
