44f6de41621e1d79821f914c674ad1215271fa87
sandratang <sandratang94@gmail.com>
2020-11-25 21:08:40 +0100

    Update AdminBanque.js

--- frontend/src/views/admin/AdminView/AdminBanque.js
+++ frontend/src/views/admin/AdminView/AdminBanque.js
@@ -9,9 +9,12 @@ import {
   CardHeader,
   makeStyles,
   Grid,
-  Divider
+  Divider,
+  FormControl,
+  InputLabel,
+  Input,
+  InputAdornment
 } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles(theme => ({
   root: {},
@@ -20,91 +23,90 @@ const useStyles = makeStyles(theme => ({
   },
   textField: {
     width: '20ch'
-  },
+  }
 }));
 
 const AdminBanque = ({ className, ...rest }) => {
   const classes = useStyles();
 
+  const [values, setValues] = React.useState({
+    amount: ''
+  });
+
+  const handleChange = prop => event => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
   return (
     <Box mt={3}>
-    <Card className={clsx(classes.root, className)} {...rest} display="flex">
-      <CardHeader title="Ton argent" />
-      <Divider />
-      <CardContent display="flex">
-        <Grid
-          container
-          display="flex"
-          flex-direction="row"
-          justify="space-between"
-          spacing={5}
-        >
-          <Grid item display="flex">
-            <form className={classes.container} noValidate>
-              <TextField
-                required
-                id="date"
-                label="Date"
-                type="date"
-                defaultValue="2020-11-30"
-                className={classes.textField}
-              />
-            </form>
+      <Card className={clsx(classes.root, className)} {...rest} display="flex">
+        <CardHeader title="Ton argent" />
+        <Divider />
+        <CardContent display="flex">
+          <Grid
+            container
+            display="flex"
+            flex-direction="row"
+            justify="space-between"
+            spacing={5}
+          >
+            <Grid item display="flex">
+              <FormControl fullWidth className={classes.textField}>
+                <InputLabel>Dernier débit</InputLabel>
+                <Input
+                  id="debit"
+                  value={values.amount}
+                  onChange={handleChange('amount')}
+                  startAdornment={
+                    <InputAdornment position="end">€</InputAdornment>
+                  }
+                />
+              </FormControl>
+            </Grid>
+            <Grid item display="flex">
+              <FormControl fullWidth className={classes.textField}>
+                <InputLabel>Dernier crédit</InputLabel>
+                <Input
+                  id="credit"
+                  value={values.amount}
+                  onChange={handleChange('amount')}
+                  startAdornment={
+                    <InputAdornment position="end">€</InputAdornment>
+                  }
+                />
+              </FormControl>
+            </Grid>
+            <Grid item display="flex">
+              <FormControl fullWidth className={classes.textField}>
+                <InputLabel>Solde</InputLabel>
+                <Input
+                  id="solde"
+                  value={values.amount}
+                  onChange={handleChange('amount')}
+                  startAdornment={
+                    <InputAdornment position="end">€</InputAdornment>
+                  }
+                />
+              </FormControl>
+            </Grid>
+            <Grid item display="flex">
+              <Box display="flex" justifyContent="flex-end" p={2}>
+                <Button
+                  size="small"
+                  variant="text"
+                  type="submit"
+                  style={{
+                    backgroundColor: '#388A36',
+                    color: 'white'
+                  }}
+                >
+                  Valider
+                </Button>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid item display="flex">
-            <form className={classes.container} noValidate>
-              <TextField
-                required
-                id="time"
-                label="Heure de coucher"
-                type="time"
-                defaultValue="18:30"
-                className={classes.textField}
-                InputLabelProps={{
-                  shrink: true
-                }}
-                inputProps={{
-                  step: 300 // 5 min
-                }}
-              />
-            </form>
-          </Grid>
-          <Grid item display="flex">
-            <form className={classes.container} noValidate>
-              <TextField
-                required
-                id="time"
-                label="Heure de réveil"
-                type="time"
-                defaultValue="06:00"
-                className={classes.textField}
-                InputLabelProps={{
-                  shrink: true
-                }}
-                inputProps={{
-                  step: 300 // 5 min
-                }}
-              />
-            </form>
-          </Grid>
-          <Grid item display="flex">
-            <Box display="flex" justifyContent="flex-end" p={2}>
-              <Button
-                size="small"
-                variant="text"
-                type="submit"
-                style={{
-                  backgroundColor: '#388A36',
-                  color: 'white'
-                }}
-              >
-                Valider
-              </Button>
-            </Box>
-          </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
     </Box>
   );
 };
