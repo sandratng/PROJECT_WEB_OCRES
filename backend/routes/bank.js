24e197c4ca96a3cacd8a7f09a4d443ea510660e6
oumar <omzoanne497@gmail.com>
2020-12-14 01:51:05 +0100

    update compte

--- backend/routes/bank.js
+++ backend/routes/bank.js
@@ -18,6 +18,18 @@ router.get("/", async function (req, res, next) {
 /* POST bank. */
 router.post("/", async function (req, res, next) {
   try {
+    console.log("Hello World");
+    const dbc = client.db("ecedashboard");
+    const collections = await dbc.collection("bank").find().toArray();
+    
+    if(collections){
+      const test=collections[collections.length-1];
+      if(test){
+        req.body.solde=test.solde+req.body.solde;
+        req.body.debit=test.debit+req.body.debit;
+        req.body.credit=test.credit+req.body.credit;
+      }
+    }
 
     const db = client.db("ecedashboard");
     const result = await db.collection("bank").insertOne(req.body);
